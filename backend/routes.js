2aac0b31005e62b518b20fa0440195ca87ba0b13
Arram Mandel <airum82@gmail.com>
2018-12-05 17:20:57 -0700

    Moved route callbacks to route file for scalability

--- backend/routes.js
+++ backend/routes.js
@@ -1,5 +1,57 @@
+const mongodb = require('mongodb')
+const mongoClient = mongodb.MongoClient
+const url = 'mongodb://localhost:27017/'
+
 module.exports = {
-  fetchWorkoutGroup: (res, req) => {
-    return res.status(200).json()
+  checkForDuplicates: (dbo, collection, name) => {
+    return dbo.collection(collection).findOne({ name: name })
+  },
+  fetchWorkoutGroup: (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    mongoClient.connect(url, async (err, db) => {
+      if(err) {
+        console.log('there was an error:', err);
+      } else {
+        const dbo = db.db('bodyGroups');
+        dbo.collection(req.params.bodyGroup).find({}).toArray((err, result) => {
+          if(err) throw err;
+          db.close()
+          return res.status(200).json(result)
+        })
+      }
+    })
+  },
+  createNewGroup: (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    const newGroup = req.params.bodyGroup;
+    mongoClient.connect(url, (err, db) => {
+      if (err) throw err;
+      const dbo = db.db('bodyGroups');
+      dbo.createCollection(newGroup, (err, response) => {
+        if (err) throw err;
+        console.log(`${newGroup} collection was created!`);
+        res.status(201).json(`${newGroup} was created!`)
+        db.close();
+      })
+    })
+  },
+  createNewExercise: (req, res) => {
+    const newExercise = req.body;
+    console.log(newExercise);
+    const confirmation = `${newExercise.name} was added!`
+    res.header('Access-Control-Allow-Origin', '*');
+    mongoClient.connect(url, async (err, db) => {
+      if (err) throw err;
+      const dbo = db.db('bodyGroups');
+      if (await checkForDuplicates(dbo, req.params.exercise, newExercise.name)) {
+        return res.status(402).send('It already exists')
+      }
+      dbo.collection(req.params.group).insertOne(newExercise, (err, res) => {
+        if (err) throw err;
+        console.log(confirmation);
+        db.close();
+        return res.status(201).json(confirmation)
+      })
+    })
   }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,73 +1,20 @@
 const express = require('express');
 const app = express();
-const mongodb = require('mongodb');
-const mongoClient = mongodb.MongoClient
-const url = 'mongodb://localhost:27017/';
 const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Lift Log';
 
-const checkForDuplicates = (dbo, collection, name) => {
-  return dbo.collection(collection).findOne({ name: name })
-}
-
 app.get('/', (req, res) => {
   res.send('Welcome Arram, shall we begin?')
 });
 
-app.get('/api/bodyGroups/:bodyGroup', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  mongoClient.connect(url, async (err, db) => {
-    if(err) {
-      console.log('there was an error:', err);
-    } else {
-      const dbo = db.db('bodyGroups');
-      dbo.collection(req.params.bodyGroup).find({}).toArray((err, result) => {
-        if(err) throw err;
-        db.close()
-        return res.status(200).json(result)
-      })
-    }
-  })
-})
-
-app.post('/api/bodyGroups/:bodyGroup', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  const newGroup = req.params.bodyGroup;
-  mongoClient.connect(url, (err, db) => {
-    if (err) throw err;
-    const dbo = db.db('bodyGroups');
-    dbo.createCollection(newGroup, (err, response) => {
-      if (err) throw err;
-      console.log(`${newGroup} collection was created!`);
-      res.status(201).json(`${newGroup} was created!`)
-      db.close();
-    })
-  })
-})
-
-app.post('/api/bodyGroups/new/:exercise', (req, res) => {
-  const newExercise = req.body;
-  console.log(newExercise);
-  const confirmation = `${newExercise.name} was added!`
-  res.header('Access-Control-Allow-Origin', '*');
-  mongoClient.connect(url, async (err, db) => {
-    if (err) throw err;
-    const dbo = db.db('bodyGroups');
-    if (await checkForDuplicates(dbo, req.params.exercise, newExercise.name)) {
-      return res.status(402).send('It already exists')
-    }
-    dbo.collection(req.params.group).insertOne(newExercise, (err, res) => {
-      if (err) throw err;
-      console.log(confirmation);
-      db.close();
-      return res.status(201).json(confirmation)
-    })
-  })
-})
+app.get('/api/bodyGroups/:bodyGroup', routes.fetchWorkoutGroup)
+app.post('/api/bodyGroups/:bodyGroup', routes.createNewGroup)
+app.post('/api/bodyGroups/new/:exercise', routes.createNewExercise)
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
