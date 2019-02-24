2382f3e073202796832999e4a1ca9653984ebeb1
Arram Mandel <airum82@gmail.com>
2019-02-24 15:36:38 -0700

    Add conditional rendering welcome message

--- frontend/src/components/Landing.vue
+++ frontend/src/components/Landing.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="landing">
+    <h2 v-if="account.uid">Welcome {{ account.email }}</h2>
+    <h2 v-else>Signup or Login to get started</h2>
+    <button v-if="account.uid" v-on:click="signOut">SignOut</button>
     <router-link to="/login">Login/SignUp</router-link>
     <router-link to="/log">view your log</router-link>
     <h1>Welcome to lift log: A workout tracker made by a lifter for me, but I guess you can use it too, enjoy!</h1>
-    <button v-if="account.uid" v-on:click="signOut">SignOut</button>
     <Options />
   </div>
 </template>
